3a79ee68dea556e81c72a9cf25723e75cc7348bc
Christian John Roa <roacj.cr@gmail.com>
2026-05-26 08:26:13 +0800

    Reapply "Introduce entities"
    This reverts commit 32a57d77941b5dd230c12976112bf1d7177d910e.

---
+++ DentalManagementSystem.Domain/Entities/Patient.cs
@@ -0,0 +1,17 @@
+﻿using DentalManagementSystem.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalManagementSystem.Domain.Entities
+{
+    public class Patient : BaseDomainEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
