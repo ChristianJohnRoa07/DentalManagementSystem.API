bcb135b8ef560539154ea0a03474c37592974acd
Christian John Roa <roacj.cr@gmail.com>
2026-05-26 09:49:06 +0800

    WIP Application project

---
+++ DentalManagementSystem.Application/Contracts/Persistence/IAppointmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using DentalManagementSystem.Application.Contracts.Persistence.Common;
+using DentalManagementSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalManagementSystem.Application.Contracts.Persistence
+{
+    public interface IAppointmentRepository : IBaseRepository<Appointment>
+    {
+        Task<Appointment> GetAppointment(Guid Id);
+        Task<List<Appointment>> GetAppointmentsByPatient(Guid PatientId);
+        Task<List<Appointment>> GetAppointmentsByProcedure(Guid ProcedureId);
+        Task<List<Appointment>> GetAppointmentsByDate(DateTime Date);
+
+    }
+}
