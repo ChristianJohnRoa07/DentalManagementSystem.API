2913ada2bb03f063e391316346459d800eb3e56e
Christian John Roa <roacj.cr@gmail.com>
2026-05-27 11:33:30 +0800

    Introduce service registration and db factory for API injection

---
+++ DentalManagementSystem.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,34 @@
+﻿using DentalManagementSystem.Application.Contracts.Persistence;
+using DentalManagementSystem.Application.Contracts.Persistence.Common;
+using DentalManagementSystem.Persistence.DatabaseContext;
+using DentalManagementSystem.Persistence.Repositories;
+using DentalManagementSystem.Persistence.Repositories.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalManagementSystem.Persistence
+{
+    public static partial class PersistenceServiceRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DentalManagementSystemDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DentalManagementApplicationConnection")));
+
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            services.AddScoped<IPatientRepository, PatientRepository>();
+            services.AddScoped<IProcedureRepository, ProcedureRepository>();
+
+            return services;
+        }
+
+
+    }
+}
