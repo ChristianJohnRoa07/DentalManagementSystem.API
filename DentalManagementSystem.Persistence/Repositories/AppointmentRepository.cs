9e6e5eefeb9752d69734e6fb166cadc762bf230a
Christian John Roa <roacj.cr@gmail.com>
2026-05-27 11:32:09 +0800

    Create repositories for entities

---
+++ DentalManagementSystem.Persistence/Repositories/AppointmentRepository.cs
@@ -0,0 +1,40 @@
+﻿using DentalManagementSystem.Application.Contracts.Persistence;
+using DentalManagementSystem.Domain.Entities;
+using DentalManagementSystem.Persistence.DatabaseContext;
+using DentalManagementSystem.Persistence.Repositories.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalManagementSystem.Persistence.Repositories
+{
+    public class AppointmentRepository : BaseRepository<Appointment>, IAppointmentRepository
+    {
+        private readonly DentalManagementSystemDbContext _dbContext;
+
+        public AppointmentRepository(DentalManagementSystemDbContext dbContext) : base(dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Appointment>> GetAppointmentsByDate(DateTime date)
+        {
+            var targetDate = date.Date;
+
+            return await _dbContext.Appointments.Where(data => data.AppointmentDateTime.Date == targetDate).ToListAsync();
+        }
+
+        public async Task<List<Appointment>> GetAppointmentsByPatient(Guid patientId)
+        {
+            return await _dbContext.Appointments.Where(data => data.Patient.Id == patientId).ToListAsync();
+        }
+
+        public async Task<List<Appointment>> GetAppointmentsByProcedure(Guid procedureId)
+        {
+            return await _dbContext.Appointments.Where(data => data.Procedure.Id == procedureId).ToListAsync();
+        }
+    }
+}
--- DentalManagementSystem.Persistence/Repositories/Common/BaseRepository.cs
+++ DentalManagementSystem.Persistence/Repositories/Common/BaseRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DentalManagementSystem.Application.Contracts.Persistence.Common;
+using DentalManagementSystem.Domain.Entities.Common;
+using DentalManagementSystem.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,43 @@ using System.Threading.Tasks;
 
 namespace DentalManagementSystem.Persistence.Repositories.Common
 {
-    public class BaseRepository<T>
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseDomainEntity
     {
+        private readonly DentalManagementSystemDbContext _dbContext;
+        
+        public BaseRepository(DentalManagementSystemDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            await _dbContext.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetById(Guid Id)
+        {
+            return await _dbContext.Set<T>().FindAsync(Id);
+        }
+
+        public async Task Update(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
