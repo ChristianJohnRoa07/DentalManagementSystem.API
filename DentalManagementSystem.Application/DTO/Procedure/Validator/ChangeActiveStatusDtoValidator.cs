a54400d3ac86554ff3215bdd6452bb11a8f768b6
Christian John Roa <roacj.cr@gmail.com>
2026-05-28 20:02:43 +0800

    Implement change active status feature

---
+++ DentalManagementSystem.Application/DTO/Procedure/Validator/ChangeActiveStatusDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalManagementSystem.Application.DTO.Procedure.Validator
+{
+    public class ChangeActiveStatusDtoValidator : AbstractValidator<ChangeActiveStatusDto>
+    {
+        public ChangeActiveStatusDtoValidator()
+        {
+            RuleFor(p => p.ProcedureId)
+               .NotEmpty().WithMessage("{PropertyName} is required.")
+               .NotNull();
+        }
+    }
+}
---
+++ DentalManagementSystem.Application/Features/Procedures/Command/ChangeActiveStatus/ChangeActiveStatusHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using DentalManagementSystem.Application.Contracts.Persistence;
+using DentalManagementSystem.Application.DTO.Procedure.Validator;
+using DentalManagementSystem.Application.DTO.Responses;
+using DentalManagementSystem.Application.Exceptions;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalManagementSystem.Application.Features.Procedures.Command.ChangeActiveStatus
+{
+    public class ChangeActiveStatusHandler : IRequestHandler<ChangeActiveStatusCommand, BaseCommandResponse>
+    {
+        private readonly IProcedureRepository _procedureRepository;
+        private readonly IMapper _mapper;
+
+        public ChangeActiveStatusHandler(IProcedureRepository procedureRepository, IMapper mapper)
+        {
+            _procedureRepository = procedureRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseCommandResponse> Handle(ChangeActiveStatusCommand request, CancellationToken cancellationToken)
+        {
+            var response = new BaseCommandResponse();
+            var validator = new ChangeActiveStatusDtoValidator();
+            var validatorResult = await validator.ValidateAsync(request.changeActiveStatusDto, cancellationToken);
+
+            if (!validatorResult.IsValid)
+            {
+                throw new BadRequestException("Validation failed", validatorResult.Errors.Select(q => q.ErrorMessage).ToList());
+            }
+
+            var procedureId = request.changeActiveStatusDto.procedureId;
+            await _procedureRepository.ChangeProcedureStatus(procedureId);
+
+            response.Success = true;
+            response.Id = procedureId;
+            response.Message = "Procedure status updated successfully.";
+
+            return response;
+        }
+    }
+}
