437548fe1e6abc279896e6ebe40990a8875c91ab
Christian John Roa <roacj.cr@gmail.com>
2026-05-28 08:59:42 +0800

    Introduce Exceptions

--- DentalManagementSystem.Application/Exceptions/NotFoundException.cs
+++ DentalManagementSystem.Application/Exceptions/NotFoundException.cs
@@ -9,5 +9,7 @@ namespace DentalManagementSystem.Application.Exceptions
     public class NotFoundException : ApplicationException
     {
         public NotFoundException(string resource) : base($"{resource} not found.") { }
+
+        public static NotFoundException Username(string? username = null) => new($"User `{(string.IsNullOrWhiteSpace(username) ? "does not exist" : username)}`");
     }
 }
