823824ec7ee112785f46e84101428bb6991bd7ce
Christian John Roa <roacj.cr@gmail.com>
2026-05-28 08:59:22 +0800

    Introduce authentication request and responses

---
+++ DentalManagementSystem.Application/DTO/Identity/LogoutResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalManagementSystem.Application.DTO.Identity
+{
+    public class LogoutResponse
+    {
+        public bool LogoutStatus { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ DentalManagementSystem.Application/DTO/Identity/RegisterRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalManagementSystem.Application.DTO.Identity
+{
+    public class RegisterRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string MiddleName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
