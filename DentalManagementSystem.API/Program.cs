3c7586b49c9170d610718ff258ab0ac4753e9ff9
Christian John Roa <roacj.cr@gmail.com>
2026-06-08 12:41:52 +0800

    Implement exception middleware handling

--- DentalManagementSystem.API/Program.cs
+++ DentalManagementSystem.API/Program.cs
@@ -1,3 +1,4 @@
+using DentalManagementSystem.API.Middlewares;
 using DentalManagementSystem.Application;
 using DentalManagementSystem.Identity;
 using DentalManagementSystem.Identity.DbContext;
@@ -20,6 +21,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
