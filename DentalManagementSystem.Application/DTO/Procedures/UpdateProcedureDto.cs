fec2a31a1cf65de3e826420f9273ec8e26c4c597
Christian John Roa <roacj.cr@gmail.com>
2026-06-08 21:08:09 +0800

    Introduce ID for update procedure dto

--- DentalManagementSystem.Application/DTO/Procedures/UpdateProcedureDto.cs
+++ DentalManagementSystem.Application/DTO/Procedures/UpdateProcedureDto.cs
@@ -8,6 +8,7 @@ namespace DentalManagementSystem.Application.DTO.Procedures
 {
     public class UpdateProcedureDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public float? Price { get; set; }
