d3940b1b352ca122aa112930c7b98b8a09ba2cd7
Christian John Roa <roacj.cr@gmail.com>
2026-06-08 12:40:22 +0800

    Include required fields

--- DentalManagementSystem.Identity/Models/ApplicationUser.cs
+++ DentalManagementSystem.Identity/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace DentalManagementSystem.Identity.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
     }
 }
